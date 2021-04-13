6666a453643e06984b5e8aef279a63b1f59d9a7b
Sam Kessler <sholomkessler1@gmail.com>
2021-04-13 13:17:35 -0400

    Testing length computation

--- Assets/Scripts/QuadraticCurve.cs
+++ Assets/Scripts/QuadraticCurve.cs
@@ -10,6 +10,8 @@ public class QuadraticCurve : Curve
     [Range(10, 200)]
     private int _Iterations;
 
+    private float curveLength;
+
     public override void Compute()
     {
         Vector2[] points = new Vector2[_Iterations];
@@ -18,10 +20,13 @@ public class QuadraticCurve : Curve
         Vector2 endPos = _EndAnchor.transform.localPosition;
         points[0] = startPos;
         points[_Iterations - 1] = endPos;
+        curveLength = 0;
         for (int i = 1; i < _Iterations - 1; i++)
         {
             points[i] = CurveFunctions.Quadratic(startPos, midPos, endPos, (float)i / _Iterations);
+            curveLength += Vector2.Distance(points[i - 1], points[i]);
         }
+        Debug.Log(curveLength);
         edgeCollider.points = points;
         lineRenderer.positionCount = _Iterations;
         lineRenderer.SetPositions(CurveFunctions.toVector3Array(points));
