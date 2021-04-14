0b7e5a7ff8417513b1835b5625dba98b305f8a7b
Reni Yeno H <70453770+Reni-Yeno-H@users.noreply.github.com>
2021-04-14 17:56:41 -0400

    Deleting foolish things

--- Assets/UI_Assistant.cs
+++ Assets/UI_Assistant.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class UI_Assistant : MonoBehaviour
 {
-    [SerializeField] private TextWriter textWriter;
+    //[SerializeField] private TextWriter textWriter;
     private Text messageText;
 
     private void Awake(){
@@ -13,9 +13,44 @@ public class UI_Assistant : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        //messageText.text = "Hello World!";
-        textWriter.AddWriter(messageText, "Hello World!", 1f);
+        messageText.text = "Hello World!";
+        //textWriter.AddWriter(messageText, "Hello World!", 1f);
     }
 
     
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// public class textWriter : MonoBehaviour
+// {
+
+//     private Text uiText;
+//     private string textToWrite;
+//     private int characterIndex;
+//     private float timePerCharacter;
+//     private float timer;
+
+//     public void AddWriter(Text uiText, string textToWrite, float timePerCharacter) {
+//         this.uiText = uiText;
+//         this.textToWrite = textToWrite;
+//         this.timePerCharacter = timePerCharacter;
+//         characterIndex = 0;
+//     }
+
+//     // Update is called once per frame
+//     private void Update()
+//     {
+//         if(uiText != null) {
+//             timer -= Time.deltaTime;
+//             if(timer <= 0f) {
+//                 //Display next character
+//                 timer += timePerCharacter;
+//                 characterIndex++;
+//                 uiText.text = textToWrite.SubString(0,characterIndex);
+//             }
+//         }
+//     }
+// }
