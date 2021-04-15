e0c5d6f81697c2e3a1cb4aa44f1870056a9fab63
Reni Yeno H <70453770+Reni-Yeno-H@users.noreply.github.com>
2021-04-14 21:41:13 -0400

    respawnAndDeath

---
+++ Assets/respawn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    public static respawn instance;
+
+    public Transform respawnPoint;
+    public GameObject carPrefab;
+
+    private void Awake(){
+        instance = this;
+    }
+    
+
+    
+    public void RespawnPoint()
+    {
+        Instantiate(carPrefab, respawnPoint.position, Quaternion.identity);
+    }
+}
