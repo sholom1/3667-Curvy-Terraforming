c96b35608e46776dfc422d0f75fe2dfa8cefa23a
Sam Kessler <sholomkessler1@gmail.com>
2021-04-24 17:46:43 -0400

    Added flag to toggle player movement

--- Assets/ScriptForLevel1/PlayerMovement.cs
+++ Assets/ScriptForLevel1/PlayerMovement.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed = 10.0f;
-    
+    public bool CanMove = true;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float move = Input.GetAxis("Horizontal");
+        if (CanMove)
+        {
+            float move = Input.GetAxis("Horizontal");
 
-        if(move < 0) 
-        GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        if(move > 0) 
-        GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
-    
+            if (move < 0)
+                GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if (move > 0)
+                GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
     }
 }
