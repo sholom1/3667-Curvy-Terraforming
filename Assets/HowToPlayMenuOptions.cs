3f5f0a8f16618ed359126d0a26a4213a85fdd9b3
ann liberman <4nnliberman@gmail.com>
2021-04-29 16:22:56 -0400

    HowToPlayMenu

---
+++ Assets/HowToPlayMenuOptions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HowToPlayMenuOptions : MonoBehaviour
+{
+   
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+
+}
