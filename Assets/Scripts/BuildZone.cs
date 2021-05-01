08920e7ca8d92ef7204525ba018d6ed11abb49d2
Sam Kessler <sholomkessler1@gmail.com>
2021-05-01 15:14:28 -0400

    Created build zone

---
+++ Assets/Scripts/BuildZone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BuildZone : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera BuildView;
+    [SerializeField]
+    private int Priority;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out PlayerMovement player) && CurveManager.instance != null)
+        {
+            CurveManager.instance.OnToggleBuildMode.AddListener(ToggleBuildMode);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out PlayerMovement player) && CurveManager.instance != null)
+        {
+            CurveManager.instance.OnToggleBuildMode.RemoveListener(ToggleBuildMode);
+        }
+    }
+    private void ToggleBuildMode(bool value)
+    {
+        BuildView.Priority = value ? Priority : -Priority;
+    }
+}
