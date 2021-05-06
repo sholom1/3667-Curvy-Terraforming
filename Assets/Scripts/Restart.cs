8390e368027215a658857201b9ec0b7aff36c348
Sam Kessler <sholomkessler1@gmail.com>
2021-05-06 15:10:39 -0400

    Player now respawn upright

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -11,6 +11,7 @@ public class Restart : MonoBehaviour
         if (BuildZone.LastBuildZone != null && PlayerMovement.instance != null && CurveManager.instance != null)
         {
             PlayerMovement.instance.transform.position = (Vector2)BuildZone.LastBuildZone.transform.position + offset;
+            PlayerMovement.instance.transform.right = Vector2.right;
             CurveManager.instance.ToggleBuildMode(false);
         }
     }
